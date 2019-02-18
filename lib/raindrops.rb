ff9340840c4f6aa123ab080df77cd8079b42ab23
Cristina María Ocaña Manzano <cristinaocana1@gmail.com>
2019-02-18 16:19:53 +0000

    Output 'Plong' when input number is divisible by 7

--- lib/raindrops.rb
+++ lib/raindrops.rb
@@ -6,6 +6,7 @@ class Raindrops
   def print_result(input_number)
     print 'Pling' if (input_number % 3).zero?
     print 'Plang' if (input_number % 5).zero?
-    print input_number unless (input_number % 3).zero? || (input_number % 5).zero?
+    print 'Plong' if (input_number % 7).zero?
+    print input_number unless (input_number % 3).zero? || (input_number % 5).zero? || (input_number % 7).zero?
   end
 end
--- spec/rainsdrops_spec.rb
+++ spec/rainsdrops_spec.rb
@@ -4,7 +4,7 @@ describe Raindrops do
   let(:raindrops) { described_class.new }
 
   describe '#print_result' do
-    it 'outputs the input number when it is not disible by 3' do
+    it 'outputs the input number when it is not disible by 3, 5 or 7' do
       expect { raindrops.print_result(34) }.to output('34').to_stdout
     end
 
@@ -16,6 +16,10 @@ describe Raindrops do
       expect { raindrops.print_result(10) }.not_to output('10').to_stdout
     end
 
+    it 'does not output the input number when it is divisible by 7' do
+      expect { raindrops.print_result(14) }.not_to output('14').to_stdout
+    end
+
     it 'outputs "Pling" when the input number is divisible by 3' do
       expect { raindrops.print_result(9) }.to output('Pling').to_stdout
     end
@@ -24,8 +28,8 @@ describe Raindrops do
       expect { raindrops.print_result(10) }.to output('Plang').to_stdout
     end
 
-    it 'outputs "PlingPlang" when the input number is divisible by 3 and 5' do
-      expect { raindrops.print_result(15) }.to output('PlingPlang').to_stdout
+    it 'outputs "Plong" when the input number is divisible by 7' do
+      expect { raindrops.print_result(14) }.to output('Plong').to_stdout
     end
   end
 end
