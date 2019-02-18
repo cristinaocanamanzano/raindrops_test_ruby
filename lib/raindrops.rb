14324b56247017ad9b0d3f76f83da91c08c32e58
Cristina María Ocaña Manzano <cristinaocana1@gmail.com>
2019-02-18 16:43:28 +0000

    Refactor logic to check if number not divisible by rules

--- lib/raindrops.rb
+++ lib/raindrops.rb
@@ -1,18 +1,27 @@
-# - if factor of 3, output 'Pling'
-# - if factor of 5, output 'Plang'
-# - if factor of 7, output 'Plong'
-
 class Raindrops
+
+  RULES = {
+    3 => "Pling",
+    5 => "Plang",
+    7 => "Plong"
+  }
+
   def print_result(input_number)
     print 'Pling' if divisible_by?(input_number, 3)
     print 'Plang' if divisible_by?(input_number, 5)
     print 'Plong' if divisible_by?(input_number, 7)
-    print input_number unless divisible_by?(input_number, 3) || divisible_by?(input_number, 5) || divisible_by?(input_number, 7)
+    print input_number if not_divisible_by_rules?(input_number)
   end
 
   private
 
   def divisible_by?(input_number, divisor)
     (input_number % divisor).zero?
+  end
+
+  def not_divisible_by_rules?(input_number)
+    divisors = []
+    RULES.each_key { |rules_number| divisors << input_number if divisible_by?(input_number, rules_number) }
+    divisors.empty?
   end
 end
