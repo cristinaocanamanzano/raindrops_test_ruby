4fa16e6e0edb1bd4aef65356ab3e539a9d6148ae
Cristina María Ocaña Manzano <cristinaocana1@gmail.com>
2019-02-18 16:03:57 +0000

    Output 'Pling' when input number is divisible by 3

--- lib/raindrops.rb
+++ lib/raindrops.rb
@@ -1,5 +1,10 @@
+# - if factor of 3, output 'Pling'
+# - if factor of 5, output 'Plang'
+# - if factor of 7, output 'Plong'
+
 class Raindrops
-  def print_result(input_numer)
-    print input_numer
+  def print_result(input_number)
+    print 'Pling' if (input_number % 3).zero?
+    print input_number unless (input_number % 3).zero?
   end
 end
--- spec/rainsdrops_spec.rb
+++ spec/rainsdrops_spec.rb
@@ -1,11 +1,19 @@
-require "raindrops"
+require 'raindrops'
 
 describe Raindrops do
-  let(:raindrops)      { described_class.new }
+  let(:raindrops) { described_class.new }
 
-  describe "#print_result" do
-    it "outputs the number taken as the input" do
-      expect { raindrops.print_result(34)}.to output("34").to_stdout
+  describe '#print_result' do
+    it 'outputs the input number when it is not disible by 3' do
+      expect { raindrops.print_result(34) }.to output('34').to_stdout
+    end
+
+    it 'does not output the input number when it is divisible by 3' do
+      expect { raindrops.print_result(9) }.not_to output('34').to_stdout
+    end
+
+    it 'outputs "Pling" when the input number is divisible by 3' do
+      expect { raindrops.print_result(9) }.to output('Pling').to_stdout
     end
   end
 end
